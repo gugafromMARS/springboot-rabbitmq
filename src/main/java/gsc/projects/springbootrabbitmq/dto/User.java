e47dc160fecf99dd6e1ab0f5b084015a769e2d65
Gonçalo <goncaloscravo@gmail.com>
2023-11-05 14:16:02 +0000

    Add: rabbitmq json configuration

---
+++ src/main/java/gsc/projects/springbootrabbitmq/dto/User.java
@@ -0,0 +1,26 @@
+package gsc.projects.springbootrabbitmq.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
